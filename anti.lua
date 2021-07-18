1364b6be66e07d9e828a97e472bcbc9cb0a3b4dd
Pattersonsoo <86987154+Pattersonsoo@users.noreply.github.com>
2021-07-19 01:46:12 +0500

    Update anti.lua

--- anti.lua
+++ anti.lua
@@ -3,17 +3,19 @@ local sampev = require 'lib.samp.events'
 local spec = false
 local inicfg = require 'inicfg'
 local dlstatus = require('moonloader').download_status
-local inicfg = require 'inicfg'
+local imgui = require 'imgui'
+local encoding = require 'encoding'
+local keys = require "vkeys"
 
 update_state = false
 
-local script_vers = 2
+local script_vers = 1
 local script_vers_text = "1.02"
 
 local update_url = "https://raw.githubusercontent.com/Pattersonsoo/test/main/update.ini"
 local update_path = getWorkingDirectory().. "/update.ini"
 
-local script_url = "https://github.com/Pattersonsoo/test/blob/main/anti.luac?raw=true"
+local script_url = "https://github.com/Pattersonsoo/test/blob/main/anti.lua?raw=true"
 local script_parh = thisScript().path
 
 
@@ -41,7 +43,7 @@ function main()
         if status == dlstatus.STATUS_ENDDOWNLOADDATA then
             updateIni = inicfg.load(nil, update_path)
             if tonumber(updateIni.info.vers) > script_vers then
-                sampAddChatMessage("Åñòü îáíîâëåíèå! Âåðñèÿ: " .. updateIni.info.vers_text, -1)
+                sampAddChatMessage("Есть обновление! Версия: " .. updateIni.info.vers_text, -1)
                 update_state = true
             end
             os.remove(update_path)
@@ -54,7 +56,7 @@ function main()
         if update_state then
             downloadUrlToFile(script_url, script_path, function(id, status)
                 if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage("Ñêðèïò óñïåøíî îáíîâëåí!", -1)
+                    sampAddChatMessage("Скрипт успешно обновлен!", -1)
                     thisScript():reload()
                 end
             end)
